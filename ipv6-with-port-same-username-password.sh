7b7f3b075264b5f6080ffe1c1bcfb83743de94b6
truongtungminh <truongtungminh@gmail.com>
2024-10-15 10:40:46 +0700

    Update ipv6-with-port-same-username-password.sh

--- ipv6-with-port-same-username-password.sh
+++ ipv6-with-port-same-username-password.sh
@@ -33,7 +33,7 @@ curl -F "file=@proxy.txt" https://file.io
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 5000
+maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -113,7 +113,7 @@ while :; do
     echo "Number out of range, try again"
   fi
 done
-LAST_PORT=$(($FIRST_PORT + 3000))
+LAST_PORT=$(($FIRST_PORT + 2000))
 echo "LAST_PORT is $LAST_PORT. Continue..."
 
 gen_data >$WORKDIR/data.txt
